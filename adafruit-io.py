ab12d6054f46e4839368150ae58553c733fe0605
blthree89 <blthree89@gmail.com>
2017-01-17 01:48:51 -0500

    fully "working" version v0.1. data is logged in nosql db, but not used for anything

--- adafruit-io.py
+++ adafruit-io.py
@@ -1,47 +1,102 @@
 # Import library and create instance of REST client.
-from Adafruit_IO import Client
-from apscheduler.schedulers.background import BackgroundScheduler
-from tinydb import TinyDB, Query
 import time
-# create scheduler and tinydb instances
-sched = BackgroundScheduler()
+
+from Adafruit_IO import Client
+from tinydb import TinyDB
+
+from pyduino import *
+
 db = TinyDB('db.json')
-lastGoodRecord = {}
+
 # function to check feeds and add to db
-# inputs and outputs the last received record
+# list of feeds to check
+tempfeeds = ['BedroomTemp', 'LivingroomTemp']
+
 def checkFeeds():
-    BedroomTemp = aio.receive('BedroomTemp')
-    recordDate = BedroomTemp.created_at.split('T')[:-1][0]
-    recordTime = BedroomTemp.created_at.split('T')[1].split('.')[0][:-3]
-    BedroomRecord = {'Location': 'Bedroom', 'Temperature': BedroomTemp.value, 'Date': recordDate, 'Time': recordTime}
-    db.insert(BedroomRecord)
-    print('Received value: {0}'.format(BedroomTemp.value))
+    for fd in tempfeeds:
+        recordTemp = aio.receive(fd)
+        recordDate = recordTemp.created_at.split('T')[:-1][0]
+        recordTime = recordTemp.created_at.split('T')[1].split('.')[0][:-3]
+        feedRecord = {'Location': fd, 'Temperature': recordTemp.value, 'Date': recordDate,
+                      'Time': recordTime}
+        db.insert(feedRecord)
+        # print('Received value: {0}'.format(recordTemp.value))
 
-    LivingRoomTemp = aio.receive('LivingroomTemp')
-    recordDate = LivingRoomTemp.created_at.split('T')[:-1][0]
-    recordTime = LivingRoomTemp.created_at.split('T')[1].split('.')[0][:-3]
-    LivingRoomRecord = {'Location': 'Living Room', 'Temperature': LivingRoomTemp.value, 'Date': recordDate, 'Time': recordTime}
-    # to be replaced with something to check for a duplicate record
-    db.insert(LivingRoomRecord)
-    print('Received value: {0}'.format(LivingRoomTemp.value))
-    #currently broken
-    #if BedroomRecord != lastGoodRecord:
-     #   db.insert(BedroomRecord)
-      #  print('Received value: {0}'.format(BedroomTemp.value))
-       # lastGoodRecord = BedroomRecord
-    print(db.all())
-    return
+        if fd == 'BedroomTemp':
+            bedroomTemp = recordTemp.value
+            print(bedroomTemp)
+
+    # SetTemp = aio.receive('SetTemp')
+
+    # print(db.all())
+    return int(bedroomTemp)
 
 
+# pass this function on or off and it will change the state
+def flipstate(cs):
+    if cs == 'ON':
+        cs = 'OFF'
+    elif cs == 'OFF':
+        cs = 'ON'
+    return cs
+
+
+# function to decide if thermostat should be on/off
+# currently uses a simple 6-degree window around the set temp
+# returns a state, either on or off
+def thermologic(target, bt, lt, cs):
+    assert target > 60
+    assert target < 90
+    if bt >= target + 3:
+        return 'OFF'
+    elif bt <= target - 3:
+        return 'ON'
+    else:
+        return cs
+
+
+#################################################
 # start adafruit IO client
 aio = Client('14737421b335461c9a194995f9b537af')
 
-# setup scheduler to check every 10 seconds then start it up
-sched.add_job(checkFeeds, 'interval', seconds=10)
-sched.start()
+a = Arduino()
+# if your arduino was running on a serial port other than '/dev/ttyACM0/'
+# declare: a = Arduino(serial_port='/dev/ttyXXXX')
 
+time.sleep(3)
+# sleep to ensure ample time for computer to make serial connection
 
-for x in range(0,100):
-    print('x=',x)
+statusPin = 12
+relayPin = 4
+a.set_pin_mode(statusPin, 'O')
+time.sleep(.5)  # need to let the arduino catch up
+# initialize the digital pins as IO
+
+time.sleep(1)
+# allow time to make connection
+
+# flash LED to show startup
+for j in range(0, 6):
+    if j % 2 == 0:
+        a.digital_write(statusPin, 1)
+    else:
+        a.digital_write(statusPin, 0)
+    time.sleep(.25)
+for j in range(0, 6):
+    if j % 2 == 0:
+        a.digital_write(relayPin, 1)
+    else:
+        a.digital_write(relayPin, 0)
+    time.sleep(.25)
+######################################
+thermostate = 'OFF'
+for x in range(0, 300):
+    # check every 5 seconds
+    if x % 5 == 0:
+        latestTemp = checkFeeds()
+        # print(latestTemp)
+        # print(thermologic(80, latestTemp, 60, thermostate))
+        thermostate = thermologic(70, latestTemp, 60, thermostate)
+        print(thermostate)
     time.sleep(1)
 
